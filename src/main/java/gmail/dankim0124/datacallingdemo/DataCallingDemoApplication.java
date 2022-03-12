27c7abb26014c88ab491d8eece4a1bfbfb5712f2
김도현 <dankim0124@gmail.com>
2022-03-12 23:25:19 +0900

    initial commit

---
+++ src/main/java/gmail/dankim0124/datacallingdemo/DataCallingDemoApplication.java
@@ -0,0 +1,13 @@
+package gmail.dankim0124.datacallingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCallingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCallingDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gmail/dankim0124/datacallingdemo/DataCallingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package gmail.dankim0124.datacallingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCallingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
